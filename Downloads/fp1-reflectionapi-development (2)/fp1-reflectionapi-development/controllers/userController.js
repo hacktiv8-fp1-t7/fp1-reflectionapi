4d23e3cef77600ddbd35faf5cb85d373e7a5c97f
MutiaMuthmainnah <mutia.muthmainnah1@gmail.com>
2023-04-27 19:03:16 +0700

    setup sempentara endpoint create

---
+++ Downloads/fp1-reflectionapi-development (2)/fp1-reflectionapi-development/controllers/userController.js
@@ -0,0 +1,75 @@
+const db = require("../database");
+const { hashPassword, comparePassword } = require("../helpers/bcrypt");
+const { generateToken } = require("../helpers/jwt");
+
+class userController {
+  static async register(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      await db.query(`insert into users (email, password) values($1, $2);`, [email, hashPassword(password)]);
+
+      const result = await db.query(`insert into users (email, password) values($1, $2);`, [email, hashPassword(password)]);
+
+      console.log(result);
+
+      const id = result.rows[0].id;
+
+      console.log(id);
+      res.status(201).json({ message: "user created", id, email });
+
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+    static async login(req,res){ 
+        try {
+          let access_token
+            const {
+              email,
+              password
+            } = req.body
+            
+            const { rows } =  await db.query(`select * from users where email = $1;`, [email])
+            console.log(rows);
+            
+            const user = {
+              id: rows[0].id,
+              email: rows[0].email
+            }
+
+            if (!user) {
+              res.status(404).json({
+                code: 404,
+                message: "User not found"
+              })
+            }else{
+              access_token = generateToken(user)
+            }
+      
+            // compare password
+            const isCorrect = comparePassword(password, rows[0].password)
+      
+            if (!isCorrect) {
+              res.status(404).json({
+                code: 401,
+                message: "Incorrect password"
+              })
+            }
+            
+            
+      
+            res.status(200).json({
+              access_token
+            })
+      
+          } catch (error) {
+            console.log(error);
+            
+          }
+    }
+}
+
+module.exports = userController;
