37d3098770dfee20e1bf483a1b9a8aff52aa0c7a
Gregorius Damar <damargregorius1002@gmail.com>
2023-04-27 18:43:52 +0700

    remove Downloads/fp1-reflectionapi-development/fp1-reflectionapi-development

--- Downloads/fp1-reflectionapi-development/fp1-reflectionapi-development/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  user: "postgres",
-  host: "localhost",
-  database: "reflection_db",
-  password: "postgre",
-  port: 5432,
-};
-
-module.exports = config;
--- Downloads/fp1-reflectionapi-development/fp1-reflectionapi-development/database/index.js
+++
@@ -1,7 +0,0 @@
-const { Pool } = require("pg");
-const config = require("../config/config");
-const pool = new Pool(config);
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
