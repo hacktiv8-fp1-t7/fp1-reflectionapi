4296f622b2ea644a505c06ee0d448c01dac27b77
MutiaMuthmainnah <mutia.muthmainnah1@gmail.com>
2023-04-27 18:05:06 +0700

    update reflectionRouter

--- Downloads/fp1-reflectionapi-development/fp1-reflectionapi-development/routers/reflectionRouter.js
+++ Downloads/fp1-reflectionapi-development/fp1-reflectionapi-development/routers/reflectionRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ReflectionsController = require("../controllers/reflectionController");
 
 router.get("/", ReflectionsController.getReflections);
-router.post("/", ReflectionsController.createReflections);
+router.post("/", ReflectionsController.createReflections);  
 router.put("/:id", ReflectionsController.updateReflectionsById);
 router.delete("/:id", ReflectionsController.deleteReflectionsById);
 
